f3e875cf73dfcfd49e8897d75d9a4f9d5d02b4a1
Robert Fiko <fiko.robert@gmail.com>
2021-01-16 20:22:39 +0100

    Login and register pages added

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Nem KOVID · Bejelentkezés</title>
+    <title>Nem KOVID · Regisztráció</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sign-in/">
 
@@ -40,12 +40,28 @@
 <main class="form-signin">
     <form>
         <img class="mb-4" src="assets/covid.png" alt="" width="72" height="72">
-        <h1 class="h3 mb-3 fw-normal">Kérlek jelentkezz be</h1>
+        <h1 class="h3 mb-3 fw-normal">Regisztráció</h1>
+
+        <label for="inputName" class="visually-hidden">Teljes név</label>
+        <input type="text" id="inputName" name="inputName" class="form-control" placeholder="Teljes név" required autofocus>
+
+        <label for="inputTaj" class="visually-hidden">TAJ szám</label>
+        <input type="number" maxlength="9" minlength="9" id="inputTaj" name="inputTaj" class="form-control" placeholder="TAJ szám" required >
+
+        <label for="inputAddress" class="visually-hidden">Értesítési cím</label>
+        <input type="text" id="inputAddress" name="inputAddress" class="form-control" placeholder="Értesítési cím" required >
+
         <label for="inputEmail" class="visually-hidden">Email cím</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Email cím" required autofocus>
+        <input type="email" id="inputEmail" name="inputEmail" class="form-control" placeholder="Email cím" required >
+
         <label for="inputPassword" class="visually-hidden">Jelszó</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Jelszó" required>
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Belépés</button>
+        <input type="password" id="inputPassword" name="inputPassword" class="form-control" placeholder="Jelszó" required>
+
+        <label for="inputPasswordAgain" class="visually-hidden">Jelszó megismétlése</label>
+        <input type="password" id="inputPasswordAgain" name="inputPasswordAgain" class="form-control" placeholder="Jelszó megismétlése" required>
+
+
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Regisztrálás</button>
         <p class="mt-5 mb-3 text-muted">Nemzeti Koronavírus Depó - 2021</p>
     </form>
 </main>
